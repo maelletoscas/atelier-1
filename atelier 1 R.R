90dd97c964152d200cc60b51e0b6ca6567375893
maelletoscas <56030216+maelletoscas@users.noreply.github.com>
2019-10-01 15:46:01 +0200

    Add files via upload

---
+++ atelier 1 R.R
@@ -0,0 +1,28 @@
+2+3
+8 - 12
+14 * 25
+-3/10
+4*sqrt(2)
+3*4
+x<-2
+x
+x+2
+sin(x)
+y<- sin(x)
+y
+pi
+exp(1)
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+length(tailles) #taille echantillon
+sd(tailles) #ecart type
+mean(tailles) #moyenne
+var(tailles) #variance
+summary(tailles) #resume
+hist(tailles) #histogramme
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+tailles.m <- tailles/100
+imc <- poids/(tailles.m^2)
+imc
+donnee <- data.frame(tailles,poids,imc)
+View(donnee)
